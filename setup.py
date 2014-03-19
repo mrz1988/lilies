0a2c5ac47139f39d7f519200edf3f78a8dc2da8c
Matt Zychowski <mrz2004@gmail.com>
2014-03-18 20:31:59 -0400

    Add setup.py generated by PyCharm

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='lilies',
+    version='0.0.1',
+    packages=['lilies', 'lilies.packages', 'lilies.packages.colorama'],
+    url='',
+    license='MIT',
+    author='Matt Zychowski',
+    author_email='mrz2004@gmail.com',
+    description='A tool for creating colorful, formatted command line output'
+)
