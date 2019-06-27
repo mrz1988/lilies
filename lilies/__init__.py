9d8bcb4630f61686ac40d86733e03e4868d430c3
mrz1988 <mrz2004@gmail.com>
2019-06-27 10:57:58 -0700

    Add sortify to exported functions

--- lilies/__init__.py
+++ lilies/__init__.py
@@ -1,5 +1,5 @@
 from .lilystring import grow, wilt, endl
-from .layouts import columnify
+from .layouts import columnify, sortify
 from .colorama_shim import no_colorama
 
 __version__ = version = VERSION = "0.0.0.7"
