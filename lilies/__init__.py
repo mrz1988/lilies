b94b5eb1580683c91863cd62dbd4356fe2e1c823
mrz1988 <mrz2004@gmail.com>
2019-07-03 11:23:14 -0700

    Add shitty __main__

--- lilies/__init__.py
+++ lilies/__init__.py
@@ -3,7 +3,9 @@ from .utils import columnify, sortify
 from .colorama_shim import no_colorama
 from .management import lilies_init
 
-__version__ = version = VERSION = "0.0.0.7"
+__version__ = "0.0.3"
+
+version = VERSION = __version__
 
 lilies_init()
 
---
+++ lilies/__main__.py
@@ -0,0 +1,21 @@
+################################################################################
+# Lilies
+# By: Matt Zychowski (copyright 2014-2019)
+#
+# A library wrapped around colorama for using colored strings in the terminal
+# window.  Provides advanced manipulation of colored strings, including accurate
+# slicing.
+#
+################################################################################
+
+from __future__ import print_function
+from . import grow, __version__
+from .test import test_all
+
+print(grow('Lilies!', 'green'))
+print(grow('A colored text formatting tool for the command line',
+           'yellow'))
+print('Version: ' + str(__version__))
+print('Author: Matt Zychowski')
+print()
+test_all()
