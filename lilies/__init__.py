f5613fac5649d4b9579df32aacc3356eef928bb8
mrz1988 <mrz2004@gmail.com>
2019-07-03 10:59:00 -0700

    Fix import

--- lilies/__init__.py
+++ lilies/__init__.py
@@ -1,5 +1,5 @@
 from .lilystring import grow, wilt, endl
-from .layouts import columnify, sortify
+from .utils import columnify, sortify
 from .colorama_shim import no_colorama
 from .management import lilies_init
 
