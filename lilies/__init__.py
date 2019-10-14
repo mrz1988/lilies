8926fff2936cf7244f9dedf74bd8bfede9775b56
mrz1988 <mrz2004@gmail.com>
2019-10-14 12:20:07 -0700

    rev version

--- lilies/__init__.py
+++ lilies/__init__.py
@@ -6,7 +6,7 @@ from .lilystring import LilyString
 from .lilyblock import LilyBlock, block
 from . import terminal
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 version = VERSION = __version__
 
